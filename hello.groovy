676bd5e534ef44f533b0b90df45faaf2c44f6185
manyosit <manyosit@users.noreply.github.com>
2016-01-14 21:58:38 +0100

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -4,8 +4,20 @@ log.info("Welcome to flintibi!")
 
 response=call.connector("db")
               .set("action","insert" )
-              .set("query", "insert into engineers (age,lastname,city) values(27,'ani','mumbai')",
+              .set("query", "insert into engineers (age,lastname,city) values(27,'ani','mumbai')")
               .sync()
+
+response2=call.connector("db")
+ .set("action","select")
+ .set("database","user")
+ .set("type","mysql")
+ .set("target","localhost")
+ .set("port",3306)
+ .set("driver","com.mysql.jdbc.Driver")
+ .set("query","select * from user")
+ .sync()
+
+log.info "" + response2
 
 log.info "did it"
 output.set("message","Hello World!")
