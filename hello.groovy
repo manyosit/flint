2271ddc86823094adf49e8c6e448ac74af0845c9
manyosit <manyosit@users.noreply.github.com>
2016-01-14 20:51:04 +0100

    Update hello.groovy

--- hello.groovy
+++ hello.groovy
@@ -2,4 +2,10 @@
 
 log.info("Welcome to flintibi!")
 
+response=call.connector("db")
+              .set("action","insert" )
+              .set("query","insert into user (name, active) values ('test', 1)")
+              .sync()
+
+log.info "did it"
 output.set("message","Hello World!")
